4fa6d2752acaccaf9518eb599b99c99f542cbb80
Miguel Carlos Arevalo <micoarevalo.10@gmail.com>
2023-05-30 15:55:33 +0800

    Landing Page Update v1.0

---
+++ includes/footer.php
@@ -0,0 +1,86 @@
+<footer class="text-center text-lg-start text-white pt-2 bg-dark">
+    <!-- Section: Links  -->
+    <section class="text-light">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold text-secondary">IBC Philippines</h6>
+            <hr class="mb-2 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px"/>
+            <p class="text-light justify-content-center">
+              IBC Philippines is a software development company located in Quezon City, Philippines. It was incorporated on July 2, 2012 and offers a wide range of software development services, including web development, mobile development, IT support, and networking consultation.
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Menu</h6>
+            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px"/>
+            <p>
+              <a href="#!" class="text-warning"style="text-decoration:none">Home</a>
+            </p>
+            <p>
+              <a href="#!" class="text-warning"style="text-decoration:none">History</a>
+            </p>
+            <p>
+              <a href="#!" class="text-warning"style="text-decoration:none">Meet the Team</a>
+            </p>
+            <p>
+              <a href="#!" class="text-warning"style="text-decoration:none">Services</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Useful links</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#" class="text-white">Home</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Become an Affiliate</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Shipping Rates</a>
+            </p>
+            <p>
+              <a href="#!" class="text-white">Help</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Contact</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p><i class="fas fa-home mr-3"></i> Quezon City, Philippines</p>
+            <p><i class="fas fa-envelope mr-3"></i> IBCPhilippines@gmail.com</p>
+            <p><i class="fas fa-phone mr-3"></i> 0948 902 4123</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3 bg-dark text-light">
+       2023 © All Rights Reserved - IBC Philippines
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
---
+++ includes/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/main.min.css">
+    <link rel="stylesheet" href="sass/styles.css">
+    <title>IBC Philippines</title>
+</head>
+<body>
+<!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="images/NEW-IBC-LOGO.png" alt="" class="img">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+              What we do
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Consulting</a></li>
+              <li><a class="dropdown-item" href="#">Software Development</a></li>
+              <li><a class="dropdown-item" href="#">Mobile Development</a></li>
+              <li><a class="dropdown-item" href="#">IT Manpower Outsourcing</a></li>
+              <li><a class="dropdown-item" href="#">Trainings and Seminars</a></li>
+            </ul>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+              Who we are
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="https://www.youtube.com">History</a></li>
+              <li><a class="dropdown-item" href="#">Meet the Team</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="form-inline">
+          <button class="btn btn-outline-primary" type="button">Contact Us</button>
+        </form>
+      </div>
+    </div>
+  </nav>
